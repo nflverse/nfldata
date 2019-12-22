babd59a734d9879ea61fac61f0913dca8c06dc5f
Lee Sharpe <15963992+leesharpe@users.noreply.github.com>
2019-12-22 08:17:55 -0800

    Added double_games() function

--- code/plays-functions.R
+++ code/plays-functions.R
@@ -410,3 +410,21 @@ apply_series_data <- function(p)
   report(paste("Series Data Complete!"))
   return(p)
 }
+
+# double games
+## takes input where each game has one row with teams as `away_team` and `home_team`
+## returns with each game having two rows listed as `team` and `opp`
+double_games <- function(g)
+{
+  g1 <- g %>% 
+    rename(team=away_team,team_score=away_score,
+           opp=home_team,opp_score=home_score) %>% 
+    mutate(location=ifelse(location == "Home","Away",location),
+           result=-1*result)
+  g2 <- g %>% 
+    rename(team=home_team,team_score=home_score,
+           opp=away_team,opp_score=away_score)
+  g <- bind_rows(g1,g2) %>% 
+    arrange(gameday,gametime,game_id)
+  return(g)
+}
