823fb9a2ffd8386e89f5178b294a376e0b26efd6
Lee Sharpe <15963992+leesharpe@users.noreply.github.com>
2020-03-28 08:42:26 -0700

    Reversing air_is_zero (non-functional)

--- code/plays-functions.R
+++ code/plays-functions.R
@@ -296,7 +296,7 @@ apply_completion_probability <- function(p,all_plays)
       filter(play == 1 & season >= s-3 & season <= s-1) %>% 
       filter(complete_pass == 1 | incomplete_pass == 1 | interception == 1) %>% 
       filter(air_yards >= -10 & !is.na(receiver_player_id) & !is.na(pass_location)) %>% 
-      mutate(air_is_zero=ifelse(air_yards,1,0))
+      mutate(air_is_zero=ifelse(air_yards,0,1))
     
     # determine CPOE formula
     cp_model <- gam(complete_pass ~ s(air_yards) + air_is_zero + factor(pass_location),
@@ -307,7 +307,7 @@ apply_completion_probability <- function(p,all_plays)
       filter(play == 1 & season == s) %>% 
       filter(complete_pass == 1 | incomplete_pass == 1 | interception == 1) %>% 
       filter(air_yards >= -10 & !is.na(receiver_player_id) & !is.na(pass_location)) %>% 
-      mutate(air_is_zero=ifelse(air_yards,1,0))
+      mutate(air_is_zero=ifelse(air_yards,0,1))
     new_data$cp <- predict.gam(cp_model,new_data)
     new_data <- new_data %>% 
       select(game_id,play_id,cp)
@@ -487,11 +487,41 @@ apply_series_data <- function(p)
 ########## FUNCTIONS TO HELP TRANSFORM DATA OR MAKE PLOTS ##########
 
 # get data from over the cap
-fetch_otc_cap_data <- function()
+fetch_tankathon_draft_order <- function()
 {
   teams <- read_csv("https://raw.githubusercontent.com/leesharpe/nfldata/master/data/teams.csv")
   otc_raw <- readLines("https://overthecap.com/salary-cap-space/")
   otc_text <- otc_raw[56]
+  otc_text <- gsub('</tr></thead><tbody><tr class="sortable">',"\n",otc_text)
+  otc_text <- gsub('</tr><tr class="sortable">',"\n",otc_text)
+  otc_text <- gsub("<[^>]+>","%",otc_text)
+  otc_text <- gsub("%+","%",otc_text)
+  otc_text <- gsub(",","",otc_text)
+  otc_text <- gsub("\\$","",otc_text)
+  otc_text <- gsub("\\(","-",otc_text)
+  otc_text <- gsub(")","",otc_text)
+  otc_split <- str_split(otc_text,"\n")[[1]][2:33]
+  otc_split <- gsub("^%","",otc_split)
+  otc_split <- gsub("%$","",otc_split)
+  cap <- as_tibble(str_split_fixed(otc_split,"%",6)) %>% 
+    mutate(V2=as.numeric(V2),V3=as.numeric(V3),V4=as.numeric(V4),
+           V5=as.numeric(V5),V6=as.numeric(V6)) %>% 
+    rename(team=V1,cap_space_actual=V2,cap_space=V3,
+           num_players=V4,active_cap_spend=V5,dead_money=V6)
+  result <- teams %>% 
+    filter(season == max(season)) %>% 
+    select(team,nickname) %>% 
+    inner_join(cap,by=c("nickname"="team")) %>% 
+    select(-nickname)
+  return(result)
+}
+
+# get data from over the cap
+fetch_otc_cap_data <- function()
+{
+  teams <- read_csv("https://raw.githubusercontent.com/leesharpe/nfldata/master/data/teams.csv")
+  otc_raw <- readLines("https://overthecap.com/salary-cap-space/")
+  otc_text <- paste(otc_raw[51:54],collapse="")
   otc_text <- gsub('</tr></thead><tbody><tr class="sortable">',"\n",otc_text)
   otc_text <- gsub('</tr><tr class="sortable">',"\n",otc_text)
   otc_text <- gsub("<[^>]+>","%",otc_text)
